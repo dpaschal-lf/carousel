650500860d3b9aece75cea88dc55ecfe1c4187c6
dpaschal <daniel.paschal@gmail.com>
2015-02-25 09:52:24 -0800

    initial commit

---
+++ carousel.js
@@ -0,0 +1,17 @@
+//when the document is ready, initilize the image set;
+document.ready(function(){
+    init_images();
+});
+
+
+function init_images(){
+    //this will set up our image area at startup
+}
+
+function prev_image(){
+    //this will shift our images so the previous image is visible
+}
+
+function next_image(){
+    //this will shift our images so the next image is visible
+}
