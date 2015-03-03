bba9f0f49b4b59414219a50a68e79b7ff40ee0d3
dpaschal <daniel.paschal@gmail.com>
2015-03-03 15:16:05 -0800

    added image looping

--- carousel.js
+++ carousel.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 });
 var picture_array = [];  //this will hold all of our pictures in an array
 var current_picture = null;  //the index of the picture that is currently visible
+var next_picture = null;
+var transition_speed = 500;
 
 function init_images(){
     //this will set up our image area at startup
@@ -24,74 +26,88 @@ function init_images(){
     });
     
 }
+function prepare_right()
+{
+    for(var i = 0; i< picture_array.length; i++) //iterate through each EXCEPT the first
+    {
+        if(i != current_picture)
+        {
+            $(picture_array[i]).css('left','100%'); //move each item to the RIGHT 
+        }
+    }
+}
+function prepare_left()
+{
+    for(var i = 0; i< picture_array.length; i++) //iterate through each EXCEPT the first
+    {
+        if(i != current_picture)
+        {
+            $(picture_array[i]).css('left','-100%'); //move each item to the LEFT 
+        }
+    }
+}
 
 function prev_image(){
     //this will shift our images so the previous image is visible
     //move my current picture to the left
     console.log("in prev image");
+    next_picture = current_picture-1;
+    if(next_picture <=0)
+    {
+        prepare_left();
+        next_picture=picture_array.length-1;
+    }
     $(picture_array[current_picture]).animate(
     {
             left:'100%',
     }, 
-    2000, 
+    transition_speed, 
     function(){
         console.log("done moving current image");
     });
     //move my next picture onto the stage
-    $(picture_array[current_picture-1]).animate(
+    $(picture_array[next_picture]).animate(
     {
             left:'0%',
     }, 
-    2000, 
+    transition_speed, 
     function(){
         console.log("done moving previous image");
     });
-    current_picture--;
+    current_picture=next_picture;
 }
 
 function next_image(){
     //this will shift our images so the next image is visible
     //move my current picture to the left
+    next_picture = current_picture+1;
+    if(next_picture >= picture_array.length)
+    {
+        prepare_right();
+        next_picture=0;
+    }
     $(picture_array[current_picture]).animate(
     {
             left:'-100%',
     }, 
-    2000, 
+    transition_speed, 
     function(){
         //alert("it's DONE!");
     });
     //move my next picture onto the stage
-    $(picture_array[current_picture+1]).animate(
+    $(picture_array[next_picture]).animate(
     {
             left:'0%',
     }, 
-    2000, 
+    transition_speed, 
     function(){
         //alert("it's DONE!");
     });
-    current_picture++;
+    current_picture=next_picture;
 }
 
 
-function test_it(){
-    $("#display_area > img").animate(
-    {
-            left:'-100%',
-    }, 
-    2000, 
-    function(){
-        //alert("it's DONE!");
-    });
-}
-function test_it2(){
-    $("#display_area > img").animate(
-        {
-            left:'0',
 
-        }, 2000, function(){
-            //alert("it's DONE!");
-        });
-}
 
 
 
